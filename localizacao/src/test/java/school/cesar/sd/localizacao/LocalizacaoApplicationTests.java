b97695d02295a97eae217b972105648ad241ea1d
fplopes1979 <fpl@cesar.school>
2022-10-28 12:04:19 -0300

    Subindo localizacao

---
+++ localizacao/src/test/java/school/cesar/sd/localizacao/LocalizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package school.cesar.sd.localizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalizacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
