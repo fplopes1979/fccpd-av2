0f723d8cd571c7b29c53d9f0e6e5c685bb98d8db
fplopes1979 <fpl@cesar.school>
2022-10-28 12:34:38 -0300

    Arrumando os prints

--- auditoria/src/main/java/school/cesar/sd/auditoria/Receiver.java
+++ auditoria/src/main/java/school/cesar/sd/auditoria/Receiver.java
@@ -9,11 +9,13 @@ public class Receiver {
 
     public void receiveMessage(String message) throws JsonProcessingException {
 
-        System.out.println("Received <" + message + ">");
-        System.out.println("Converting from JSON to Java Object...");
+        System.out.println("Mensagem recebida: <" + message + ">" + " em formato JSON.");
+        System.out.println("Fazendo o unmarshalling...");
 
         ObjectMapper mapper = new ObjectMapper();
         Message msg = mapper.readValue(message, Message.class);
-        System.out.println(msg.getEstacao());
+        System.out.println("Conteúdo da mensagem: Estação " + msg.getEstacao() + "| Timestamp " + msg.getTimestamp() + "| Evento " + msg.getEvento());
+        System.out.println("-----------------------------------------------------");
+        System.out.println();
     }
 }
